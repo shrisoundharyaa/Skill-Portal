18d5682e9ba52dbf26401e841208a68f05cd40f2
shrisoundharyaa <shrisoundharyaa@gmail.com>
2024-12-24 09:45:40 +0530

    deleted the file and creating a new project

--- src/pages/faculty/FacultyDashboard.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function FacultyDashboard() {
-  
-}
-
-export default FacultyDashboard;
--- src/pages/student/StudentDashboard.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function StudentDashboard() {
-  
-}
-
-export default StudentDashboard;
